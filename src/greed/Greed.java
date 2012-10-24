2d05d17ee0befd8b911bc3a30b06164586733aff
Ian <ian@foo.bar>
2012-10-24 23:33:40 +0100

    Make the test pass by basing the 5 scores 50 on the 1 scores 100 code.

--- src/greed/Greed.java
+++ src/greed/Greed.java
@@ -14,8 +14,13 @@ public class Greed
         int score = 0;
 
         for (int i = 0; i < rolls.length; i++)
+        {
             if (rolls[i] == 1)
                 score += 100;
+
+            if (rolls[i] == 5)
+                score += 50;
+        }
 
         return score;
     }
