3e3a03423310053203dd6042b1046dfe5ffc9f2c
Ian <ian@foo.bar>
2012-10-25 17:56:35 +0100

    Add next failing test: tripleTwosScore200()

--- tests/greed/GreedTest.java
+++ tests/greed/GreedTest.java
@@ -58,6 +58,13 @@ public class GreedTest
         assertThatScoreIs(1100);
     }
 
+    @Test
+    public void tripleTwosScore200()
+    {
+        roll(2, 2, 2, 6, 3);
+        assertThatScoreIs(200);
+    }
+
     private void roll(int die1, int die2, int die3, int die4, int die5)
     {
         greed.roll(new int[] {die1, die2, die3, die4, die5});
