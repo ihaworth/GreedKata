5454f414e86f66716bf4d3c2c3cc7bbd62149d86
Ian <ian@foo.bar>
2012-10-25 18:38:06 +0100

    Add next failing test: tripleSixesScore600()

--- tests/greed/GreedTest.java
+++ tests/greed/GreedTest.java
@@ -86,6 +86,13 @@ public class GreedTest
         assertThatScoreIs(500);
     }
 
+    @Test
+    public void tripleSixesScore600()
+    {
+        roll(6, 6, 6, 4, 2);
+        assertThatScoreIs(600);
+    }
+
     private void roll(int die1, int die2, int die3, int die4, int die5)
     {
         greed.roll(new int[] {die1, die2, die3, die4, die5});
