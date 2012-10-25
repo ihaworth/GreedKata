d6a5099209dfe5f7c684e5ff71b508318b1d0d60
Ian <ian@foo.bar>
2012-10-25 18:15:22 +0100

    Refactor to reduce duplication between triple cases.

--- src/greed/Greed.java
+++ src/greed/Greed.java
@@ -5,6 +5,9 @@ public class Greed
     private static final int MAX_FACE_VALUE = 6;
 
     private int[] rolls;
+    private int[] faceValueCounts;
+
+    private int score;
 
     public void roll(int[] rolls)
     {
@@ -13,28 +16,37 @@ public class Greed
 
     public int score()
     {
-        int score = 0;
+        score = 0;
 
-        int[] faceValueCounts = new int[MAX_FACE_VALUE + 1];
+        faceValueCounts = new int[MAX_FACE_VALUE + 1];
 
         for (int i = 0; i < rolls.length; i++)
             faceValueCounts[rolls[i]]++;
 
-        if (faceValueCounts[1] >= 3)
-        {
-            score += 1000;
-            faceValueCounts[1] -= 3;
-        }
-
-        if (faceValueCounts[2] >= 3)
-        {
-            score += 200;
-            faceValueCounts[2] -= 3;
-        }
+        scoreTriple(1);
+        scoreTriple(2);
 
         score += faceValueCounts[1] * 100;
         score += faceValueCounts[5] * 50;
 
         return score;
     }
+
+    private int scoreTriple(int faceValue)
+    {
+        if (faceValueCounts[faceValue] >= 3)
+        {
+            score += tripleBonus(faceValue);
+            faceValueCounts[faceValue] -= 3;
+        }
+        return score;
+    }
+
+    private int tripleBonus(int faceValue)
+    {
+        if (faceValue == 1)
+            return 1000;
+        return 200;
+    }
 }
+
