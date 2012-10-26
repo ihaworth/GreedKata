3e2bb043599edbdf9b08d09741ced75f532572fe
Ian <ian@foo.bar>
2012-10-26 09:21:27 +0100

    Some final refactoring to make the code more readable.

--- src/greed/Greed.java
+++ src/greed/Greed.java
@@ -4,42 +4,48 @@ public class Greed
 {
     private static final int MAX_FACE_VALUE = 6;
 
-    private int[] rolls;
-    private int[] faceValueCounts;
-
-    private int score;
+    private int[] faceValueCounts = new int[MAX_FACE_VALUE + 1];
+    private int score = 0;
 
     public void roll(int[] rolls)
     {
-        this.rolls = rolls;
+        countDice(rolls);
+    }
+
+    private void countDice(int[] rolls)
+    {
+        for (int i = 0; i < rolls.length; i++)
+            faceValueCounts[rolls[i]]++;
     }
 
     public int score()
     {
-        score = 0;
-
-        faceValueCounts = new int[MAX_FACE_VALUE + 1];
-
-        for (int i = 0; i < rolls.length; i++)
-            faceValueCounts[rolls[i]]++;
-
-        for (int faceValue = 1; faceValue <= MAX_FACE_VALUE; faceValue++)
-            scoreTriple(faceValue);
-
-        score += faceValueCounts[1] * 100;
-        score += faceValueCounts[5] * 50;
+        scoreTriples();
+        scoreSingleOnes();
+        scoreSingleFives();
 
         return score;
     }
 
+    private void scoreTriples()
+    {
+        for (int faceValue = 1; faceValue <= MAX_FACE_VALUE; faceValue++)
+            scoreTriple(faceValue);
+    }
+
     private int scoreTriple(int faceValue)
     {
-        if (faceValueCounts[faceValue] >= 3)
+        if (triplePresent(faceValue))
         {
             score += tripleBonus(faceValue);
             faceValueCounts[faceValue] -= 3;
         }
         return score;
+    }
+
+    private boolean triplePresent(int faceValue)
+    {
+        return faceValueCounts[faceValue] >= 3;
     }
 
     private int tripleBonus(int faceValue)
@@ -48,5 +54,14 @@ public class Greed
             return 1000;
         return faceValue * 100;
     }
-}
 
+    private void scoreSingleOnes()
+    {
+        score += faceValueCounts[1] * 100;
+    }
+
+    private void scoreSingleFives()
+    {
+        score += faceValueCounts[5] * 50;
+    }
+}
